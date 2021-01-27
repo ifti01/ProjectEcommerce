f51fac3903810ec411635e6cb1028094bcdd4209
Iftekhar Fahim <iftekhar.fahim97@gmail.com>
2021-01-27 23:58:16 +0600

    Custom url and Application Configurations

---
+++ FinalProject.Services/ConfigurationsService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FinalProject.Database;
+using FinalProject.Entities;
+
+namespace FinalProject.Services
+{
+    public class ConfigurationsService
+    {
+        public Config GetConfig(string key)
+        {
+            using (var context = new CBContext())
+            {
+                return context.Configurations.Find(key);
+            }
+
+        }
+    }
+}
---
+++ configurationqueries.sql
@@ -0,0 +1,7 @@
+select * from Configs
+
+insert into Configs ([Key],[value]) values ('FacebookURL','https://www.facebook.com/iftekhar.fahim.73')
+
+insert into Configs ([Key],[value]) values ('CompanyAddress','IBBL')
+
+insert into Configs ([Key],[value]) values ('CompanyPhone','01748277100')
